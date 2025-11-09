0a7c59a0ad14a73af4bdc91a6a35cea97a05396a
DKuss1994 <kussdennis@web.de>
2025-11-09 19:03:13 +0100

    test for check if all in the field dead

--- src/test/java/org/example/GameOfLiveTest.java
+++ src/test/java/org/example/GameOfLiveTest.java
@@ -14,11 +14,28 @@ public class GameOfLiveTest {
         height = 10;
         weidht = 10;
 
+
         boolean [][] field = creatField(height,weidht);
         //prüft ob länge gleich länge ist
         assertThat(field.length).isEqualTo(height);
         assertThat(field[0].length).isEqualTo(weidht);
 
+
+    }
+    @Test
+    void fieldShouldDead(){
+        int height;
+        int weidht;
+        height = 10;
+        weidht = 10;
+
+        boolean[][] field = creatField(height,weidht);
+        for(int x = 0; x<field.length;x++){
+            for(int y = 0; y<field[x].length;y++){
+                assertThat(field[y][x]).isEqualTo(false);
+
+            }
+        }
 
     }
 
